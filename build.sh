9c3cfd69cc0a7927829ba12b3c417d9f6efc408c
Adam Drew <addrew@redhat.com>
2024-05-09 09:57:13 -0400

    Building works now

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ rm *.tgz
 cd ..
 cd ..
 yarn workspace @backstage/plugin-catalog-backend-module-ldap export-dynamic
-cd plugins/catalog-backend-module-ldap
+cd plugins/catalog-backend-module-ldap/dist-dynamic
 npm pack
+echo "sha256-"$(shasum -a 256 *.tgz | awk '{print $1}' | xxd -r -p | base64)
